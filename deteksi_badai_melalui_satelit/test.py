dc5beebd5f9851195dd79886a06d5bc7bbf745c2
anisafebriana0 <annisafebriana678@gmail.com>
2024-06-11 22:49:13 +0700

    add

--- deteksi_badai_melalui_satelit/test.py
+++ deteksi_badai_melalui_satelit/test.py
@@ -4,19 +4,24 @@ from tensorflow.keras.preprocessing import image
 import os
 import glob
 
+#fungsi ini membuat model yang telah dilatih dari file yang ditentukan
 def load_model(model_path='mymodel.h5'):
     # Load the trained model
     model = tf.keras.models.load_model(model_path)
     return model
+
+#fungsi ini untuk memproses gambar dari path yang diberikan
+#gambar diubah ukurannya menjadi 'img_size' x 'img_size'
 
 def preprocess_image(img_path, img_size):
     # Load and preprocess the image
     img = image.load_img(img_path, target_size=(img_size, img_size))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array / 255.0  # Normalize the image
+    img_array = img_array / 255.0  # Normalize the image, gambar diubah menjadi array dan dinormalisasi dengan membagi nilai pixelnya 225
     return img_array
 
+#memproses gambar dan membuat prediksi menggunakan model yang dimuat
 def predict_image(model, img_path, img_size=128):
     img_array = preprocess_image(img_path, img_size)
     prediction = model.predict(img_array)
@@ -32,7 +37,9 @@ if __name__ == '__main__':
     # Predict the image
     prediction = predict_image(model, img_path)
 
-    # Print the prediction
+    # Print the prediction,mencetak hasil prediksi,
+    # jika nilai prediksi lebih besar dari 0,5 maka dianggap "damage"
+    # jika nilai prediksi kurang dari atau sama dengan 0,5 maka dianggap "not damage"
     if prediction[0] > 0.5:
         print(f"Prediction: Damage ({prediction[0][0]})")
     else:
--- deteksi_badai_melalui_satelit/train.py
+++ deteksi_badai_melalui_satelit/train.py
@@ -1,13 +1,18 @@
+#mengimpor libary tensorflow
 import tensorflow as tf
 
+#fungsi preprocess
+#enormalisasi gambar dengan membagi nilai pixelnya dengan 255.
 def preprocess(image, label):
     image = tf.cast(image, tf.float32) / 255.0
     return image, label
 
+#Mendefinisikan ukuran gambar
 def solution_model():
     IMG_SIZE = 128
     BATCH_SIZE = 64
 
+#Memuat dataset gambar untuk pelatihan (train_ds) dan validasi (val_ds) dari direktori yang ditentukan
     train_ds = tf.keras.preprocessing.image_dataset_from_directory(
         directory='train/',
         image_size= (IMG_SIZE, IMG_SIZE)
@@ -18,12 +23,15 @@ def solution_model():
         image_size=  (IMG_SIZE, IMG_SIZE)
         , batch_size=BATCH_SIZE)
 
+#memproses dataset
+#Memetakan fungsi preprocess ke dataset pelatihan dan validasi.
     train_ds = train_ds.map(
         preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE).prefetch(
         tf.data.experimental.AUTOTUNE)
     val_ds = val_ds.map(
         preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
+#membangun model
     model = tf.keras.models.Sequential([
 
         tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3)),
@@ -43,6 +51,7 @@ def solution_model():
         metrics=['accuracy']
     )
 
+#Melatih model menggunakan dataset pelatihan dan validasi selama 10 epoch
     model.fit(
 
         train_ds,
@@ -50,6 +59,7 @@ def solution_model():
         epochs=10
     )
 
+#menyimpan model
     return model
 
 if __name__ == '__main__':
